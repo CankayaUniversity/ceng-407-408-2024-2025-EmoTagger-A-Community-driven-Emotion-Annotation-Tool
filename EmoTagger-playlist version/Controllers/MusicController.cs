9da35ddd470bffcaa7b9c7ec9f2bca6b2026ec1a
hamza waheed <138822556+Hamza1799@users.noreply.github.com>
2025-03-28 13:39:32 +0300

    Creating playlist with UserId refrence

---
+++ EmoTagger-playlist version/Controllers/MusicController.cs
@@ -0,0 +1,33 @@
+﻿using EmoTagger.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmoTagger.Controllers
+{
+    public class MusicController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MusicController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+    public async Task<IActionResult> SearchMusic(string searchTerm)
+    {
+        var musics = await _context.Music
+            .Where(m => m.title.Contains(searchTerm) || m.artist.Contains(searchTerm))
+            .ToListAsync();
+
+        return Json(musics);
+    }
+
+        public IActionResult Player()
+        {
+            var songs = _context.Music.ToList(); // tüm şarkılar
+            return View(songs);
+        }
+    }
+
+}
---
+++ EmoTagger-playlist version/ViewComponents/NowPlayingViewComponent.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EmoTagger.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmoTagger.ViewComponents
+{
+    public class NowPlayingViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NowPlayingViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // Get the current/latest music track
+            var latestMusic = await _context.Music
+                .OrderByDescending(m => m.createdat)
+                .FirstOrDefaultAsync();
+
+            // Get all music tracks ordered by creation date
+            var allTracks = await _context.Music
+                .OrderByDescending(m => m.createdat)
+                .ToListAsync();
+
+            // Create a view model that includes both the current track and all tracks
+            var viewModel = new NowPlayingViewModel
+            {
+                CurrentTrack = latestMusic,
+                AllTracks = allTracks
+            };
+
+            return View(viewModel);
+        }
+    }
+
+    // View model to hold both current track and playlist data
+    public class NowPlayingViewModel
+    {
+        public Music CurrentTrack { get; set; }
+        public List<Music> AllTracks { get; set; }
+    }
+}
